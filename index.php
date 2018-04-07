0e5feac87fdc97bde14c524d1d344127be5dde31
Canta <canta@canta.com.ar>
2018-04-06 23:34:43 -0300

    FIX: ahora el fondo anda en webgl1, y detecta entre las dos versiones.

--- index.php
+++ index.php
@@ -218,16 +218,14 @@
   </head>
   <body class="total" onresize="resize">
     <img src="images/loading.png" id="loading-image" class="total loading-image"/>
-    <script id="vertex-shader" type="x-shader/x-vertex">#version 300 es
-      precision mediump sampler3D;
-      in vec4 Vertex;
+    <script id="vertex-shader-webgl1" type="x-shader/x-vertex">
+      attribute vec4 Vertex;
       void main()
       {
           gl_Position = Vertex;
       }
     </script>
-    <script id="fragment-shader" type="x-shader/x-fragment">#version 300 es
-      precision mediump sampler3D;
+    <script id="fragment-shader-webgl1" type="x-shader/x-fragment">
       precision highp float;
       precision highp int;
       
@@ -238,17 +236,29 @@
       uniform float iTime;
       uniform float iTimeDelta;
       
+      // Canta's polyfills
+      
+        int mod(int a, int b) 
+        {
+          return int(a - b * (a / b));
+        }
+      
       vec4 getRand(vec2 pos)
       {
-          vec2 rres=vec2(textureSize(iChannel1,0));
-          return textureLod(iChannel1,pos/rres,0.);
+          //vec2 rres=vec2(textureSize(iChannel1,0));
+          vec2 rres=vec2(iResolution.xy);
+          //return textureLod(iChannel1,pos/rres,0.);
+          return vec4(0.,0.,0.,0.);
       }
 
       vec4 getRand(int idx)
       {
-          ivec2 rres=textureSize(iChannel1,0);
-          idx=idx%(rres.x*rres.y);
-          return texelFetch(iChannel1,ivec2(idx%rres.x,idx/rres.x),0);
+          //ivec2 rres=textureSize(iChannel1,0);
+          ivec2 rres=ivec2(iResolution.xy);
+          //idx=idx%(rres.x*rres.y);
+          idx = mod(idx , int(rres.x*rres.y));
+          //return texelFetch(iChannel1,ivec2(idx%rres.x,idx/rres.x),0);
+          return texture2D(iChannel1, vec2(float(mod(idx,int(rres.x))),float(idx/rres.x)));
       }
 
 
@@ -361,6 +371,161 @@
               
           staticVal *= bottomStaticOpt;
         
+        float red   =   texture2D(  iChannel0,  vec2(uv.x + xOffset -0.01*rgbOffsetOpt,y)).r+staticVal;
+        float green =   texture2D(  iChannel0,  vec2(uv.x + xOffset, y)).g+staticVal;
+        float blue  =   texture2D(  iChannel0,  vec2(uv.x + xOffset +0.01*rgbOffsetOpt,y)).b+staticVal;
+        
+        float time = floor(iTime * 16.0) / 16.0;
+        vec3 color = vec3(red,green,blue) ;
+        float scanline = sin(uv.y*800.0)* 0.1 *scalinesOpt;
+        color -= scanline;
+        
+        vec2 pos = ( fragCoord.xy / iResolution.xy );
+        color *= sin(pos.x*3.15);
+        color *= sin(pos.y*3.);
+        color *= .9;
+        
+        fragColor = vec4(color,1.0);
+        return fragColor;
+      }
+
+      //out vec4 myFragColor;
+      void main()
+      {
+          //myFragColor = mainImage2(myFragColor,gl_FragCoord.xy);
+          //myFragColor = mainImage3(myFragColor,gl_FragCoord.xy);
+          //myFragColor = vec4(myFragColor.xyz, 1.);
+          gl_FragColor = mainImage4(gl_FragColor,gl_FragCoord.xy);
+          //myFragColor = mainImage5(myFragColor,gl_FragCoord.xy);
+          //myFragColor = vec4(myFragColor.xy, getRand(int(myFragColor.x)));
+          //mainImage(myFragColor,gl_FragCoord.xy);
+          //myFragColor = texture(iChannel0,gl_FragCoord.xy/iResolution.xy);
+      }
+    </script>
+    <script id="vertex-shader-webgl2" type="x-shader/x-vertex">#version 300 es
+      precision mediump sampler3D;
+      in vec4 Vertex;
+      void main()
+      {
+          gl_Position = Vertex;
+      }
+    </script>
+    <script id="fragment-shader-webgl2" type="x-shader/x-fragment">#version 300 es
+      precision mediump sampler3D;
+      precision highp float;
+      precision highp int;
+      
+      uniform sampler2D iChannel0;
+      uniform sampler2D iChannel1;
+      uniform vec3  iResolution;
+      uniform int   iFrame;
+      uniform float iTime;
+      uniform float iTimeDelta;
+      
+      vec4 getRand(vec2 pos)
+      {
+          vec2 rres=vec2(textureSize(iChannel1,0));
+          return textureLod(iChannel1,pos/rres,0.);
+      }
+      vec4 getRand(int idx)
+      {
+          ivec2 rres=textureSize(iChannel1,0);
+          idx=idx%(rres.x*rres.y);
+          return texelFetch(iChannel1,ivec2(idx%rres.x,idx/rres.x),0);
+      }
+      // change these values to 0.0 to turn off individual effects
+      float vertJerkOpt = 1.0;
+      float vertMovementOpt = 1.0;
+      float bottomStaticOpt = 1.0;
+      float scalinesOpt = 1.0;
+      float rgbOffsetOpt = 1.0;
+      float horzFuzzOpt = 1.0;
+      // Noise generation functions borrowed from: 
+      // https://github.com/ashima/webgl-noise/blob/master/src/noise2D.glsl
+      vec3 mod289(vec3 x) {
+        return x - floor(x * (1.0 / 289.0)) * 289.0;
+      }
+      vec2 mod289(vec2 x) {
+        return x - floor(x * (1.0 / 289.0)) * 289.0;
+      }
+      vec3 permute(vec3 x) {
+        return mod289(((x*34.0)+1.0)*x);
+      }
+      float snoise(vec2 v)
+        {
+        const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
+                            0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
+                           -0.577350269189626,  // -1.0 + 2.0 * C.x
+                            0.024390243902439); // 1.0 / 41.0
+      // First corner
+        vec2 i  = floor(v + dot(v, C.yy) );
+        vec2 x0 = v -   i + dot(i, C.xx);
+      // Other corners
+        vec2 i1;
+        //i1.x = step( x0.y, x0.x ); // x0.x > x0.y ? 1.0 : 0.0
+        //i1.y = 1.0 - i1.x;
+        i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+        // x0 = x0 - 0.0 + 0.0 * C.xx ;
+        // x1 = x0 - i1 + 1.0 * C.xx ;
+        // x2 = x0 - 1.0 + 2.0 * C.xx ;
+        vec4 x12 = x0.xyxy + C.xxzz;
+        x12.xy -= i1;
+      // Permutations
+        i = mod289(i); // Avoid truncation effects in permutation
+        vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
+          + i.x + vec3(0.0, i1.x, 1.0 ));
+        vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
+        m = m*m ;
+        m = m*m ;
+      // Gradients: 41 points uniformly over a line, mapped onto a diamond.
+      // The ring size 17*17 = 289 is close to a multiple of 41 (41*7 = 287)
+        vec3 x = 2.0 * fract(p * C.www) - 1.0;
+        vec3 h = abs(x) - 0.5;
+        vec3 ox = floor(x + 0.5);
+        vec3 a0 = x - ox;
+      // Normalise gradients implicitly by scaling m
+      // Approximation of: m *= inversesqrt( a0*a0 + h*h );
+        m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
+      // Compute final noise value at P
+        vec3 g;
+        g.x  = a0.x  * x0.x  + h.x  * x0.y;
+        g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+        return 130.0 * dot(m, g);
+      }
+      float staticV(vec2 uv) {
+          float staticHeight = snoise(vec2(9.0,iTime*1.2+3.0))*0.3+5.0;
+          float staticAmount = snoise(vec2(1.0,iTime*1.2-6.0))*0.1+0.3;
+          float staticStrength = snoise(vec2(-9.75,iTime*0.6-3.0))*2.0+2.0;
+        return (1.0-step(snoise(vec2(5.0*pow(iTime,2.0)+pow(uv.x*7.0,1.2),pow((mod(iTime,100.0)+100.0)*uv.y*0.3+3.0,staticHeight))),staticAmount))*staticStrength;
+      }
+      vec4 mainImage4( vec4 fragColor, in vec2 fragCoord )
+      {
+        vec2 uv =  fragCoord.xy/iResolution.xy;
+        
+        float jerkOffset = (1.0-step(snoise(vec2(iTime*1.3,5.0)),0.8))*0.05;
+        
+        float fuzzOffset = snoise(vec2(iTime*15.0,uv.y*80.0))*0.003;
+        float largeFuzzOffset = snoise(vec2(iTime*1.0,uv.y*25.0))*0.004;
+          
+          float vertMovementOn = (1.0-step(snoise(vec2(iTime*0.2,8.0)),0.4))*vertMovementOpt;
+          float vertJerk = (1.0-step(snoise(vec2(iTime*1.5,5.0)),0.6))*vertJerkOpt;
+          float vertJerk2 = (1.0-step(snoise(vec2(iTime*5.5,5.0)),0.2))*vertJerkOpt;
+          float yOffset = abs(sin(iTime)*4.0)*vertMovementOn+vertJerk*vertJerk2*0.3;
+          float y = mod(uv.y+yOffset,1.0);
+          
+        
+        float xOffset = (fuzzOffset + largeFuzzOffset) * horzFuzzOpt;
+          
+          float staticVal = 0.0;
+         
+          for (float y = -1.0; y <= 1.0; y += 1.0) {
+              float maxDist = 5.0/200.0;
+              float dist = y/200.0;
+            staticVal += staticV(vec2(uv.x,uv.y+dist))*(maxDist-abs(dist))*1.5;
+          }
+              
+          staticVal *= bottomStaticOpt;
+        
         float red 	=   texture(	iChannel0, 	vec2(uv.x + xOffset -0.01*rgbOffsetOpt,y)).r+staticVal;
         float green = 	texture(	iChannel0, 	vec2(uv.x + xOffset,	  y)).g+staticVal;
         float blue 	=	texture(	iChannel0, 	vec2(uv.x + xOffset +0.01*rgbOffsetOpt,y)).b+staticVal;
@@ -378,7 +543,6 @@
         fragColor = vec4(color,1.0);
         return fragColor;
       }
-
       out vec4 myFragColor;
       void main()
       {
@@ -425,17 +589,32 @@
         resize();
         gl = null;
         
-        try{
-          gl = glCanvas.getContext("webgl2");
-        } catch (e) {
-          console.log("No hay webgl2 en este dispositivo.");
-        }
+        let contexts = [
+          { name: "webgl2", meta: "webgl2"},
+          { name: "experimental-webgl2", meta: "webgl2"},
+          { name: "webgl", meta: "webgl1"},
+          { name: "experimental-webgl", meta: "webgl1"}
+        ];
         
+        let cont = null;
+        for (let i = 0; i < contexts.length; i++) {
+          cont = contexts[i];
+          try{
+            gl = glCanvas.getContext(cont.name);
+            if ( gl !== null) {
+              break;
+            }
+          } catch (e) {
+            console.log("No hay contexto " + cont.name + " en este dispositivo.");
+          }
+        }
+      
         if (gl !== null) {
+          console.log("Contexto para los shaders: ", cont);
           shaderProgram = initShaderProgram(
             gl, 
-            document.getElementById("vertex-shader").innerHTML, 
-            document.getElementById("fragment-shader").innerHTML
+            document.getElementById("vertex-shader-" + cont.meta).innerHTML, 
+            document.getElementById("fragment-shader-" + cont.meta).innerHTML
           );
           
           programas = [
@@ -764,14 +943,13 @@
     <div id="no-tenes-webgl-container" class="total seccion">
       <div class="cuerpo-seccion">
         <p>Hola.</p>
-        <p>Este sitio usa WebGL2. Es una tecnología para hacer cosas tridimensionales, y con eso le hice un fondo animado re bonito a la página.</p>
+        <p>Este sitio usa WebGL. Es una tecnología para hacer cosas tridimensionales, y con eso le hice un fondo animado re bonito a la página.</p>
         <p>Pero tu compu no tiene esa tecnología. Puede ser porque es viejita, o puede ser porque no la actualizás, o bien puede ser porque los dueños de la compu (que son los dueños del software y el hardware, no vos) no te la quieren actualizar.</p>
         <p>Por esa razón, estás viendo en el fondo una imagen triste y gris de un señor que trabaja sólo con una computadora vieja, perdido en el tiempo, y sin esperanzas.</p>
         <p></p>
-        <p>Estoy trabajando para hacer que el código de esta página ande también en otras compus más desactualizadas, con WebGL1. Pero mientras tanto, vas a tener que conformarte con esto que estás viendo.</p>
-        <p>En caso de tener dudas sobre qué le puede estar pasando a tu compu, te recomiendo este link: <a target="_blank" href="https://get.webgl.org/webgl2/">https://get.webgl.org/webgl2/</a>.</p>
+        <p>En caso de tener dudas sobre qué le puede estar pasando a tu compu, te recomiendo arrancar por este link: <a target="_blank" href="https://get.webgl.org">https://get.webgl.org</a>.</p>
         <p></p>
-        <p style="text-align:center;"><button onclick="seccion_handler('atras');">Entendí</button></p>
+        <p style="text-align:center;"><button onclick="seccion_handler('atras');"> OK </button></p>
       </div>
     </div>
     <div id="que-container" class="total seccion">
